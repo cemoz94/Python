8dec73a26fa1ac9276e2bac611d4f69781813256
cemoz94 <cmzzz94@gmail.com>
2023-03-09 22:10:09 +0300

    Odev1
    Odev1

---
+++ odev1.py
@@ -0,0 +1,56 @@
+# Soru 1 - Python Veri Tipleri
+
+# 1-Metin(String) Veri Tipleri
+# str = Metinler için kullanılan veri tipidir. Çift tırnak içerisinde yazılırlar.
+
+# 2-Sayısal Veri Tipleri
+# int = Tam sayılar için kullanılan veri tipidir.
+# float = Ondalıklı sayılar için kullanılan veri tipidir.
+
+# 3-Boolean Veri Tipleri
+# bool = True(1) ya da False(0) cevapları verir.
+
+# 4-Sequence(sıralama) Veri Tipleri
+# list = Sıralamak için kullanılır. Farklı türler barındırabilir. Köşeli parantezle kullanılır.
+# tuple = list'e benzerdir. Ama yapısında değişiklik yapılamaz.
+# range = Sayısal sıralama yapar.
+
+# 5-Mapping(haritalama) Veri Tipleri
+# dict = Key-value yani anahtar-değer ilişkisi vardır.
+
+# 6-Set Veri Tipleri
+# set = Print ettiğimizde her seferinde farklı bir şekilde çıkan sıralamalardır.
+# frozenset = İçine ekleme, çıkarma yapılamayan donmuş küme setleridir.
+
+# 7-Binary Veri Tipleri
+# bytes
+# bytearray
+# memoryview
+
+
+
+# Soru 2 
+
+# *Kurslarım, Tüm Kurslar, Kariyer, Sık Sorulan Sorular = String
+# *Kategori içerisindekiler, Eğitmen içerisindekiler = list
+# *% tamamlamadaki sayılar = int
+
+
+
+# Soru 3
+
+kariyerFormDoldur = True
+if(kariyerFormDoldur):
+    print("Lütfen formu doldurunuz.")
+else:
+    print("Zaten yanıt verdiniz.")
+
+    
+
+
+
+
+
+
+
+
